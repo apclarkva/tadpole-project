42d89e8953b8e5d31d22def829062abfbef65d3b
apclarkva <apclarkva@gmail.com>
2018-11-29 13:52:04 -0500

    update scripts

--- baseline.r
+++ baseline.r
@@ -1,15 +1,14 @@
-inputTrainingData <- read.csv("proposal/TADPOLE_InputData.csv")
+library(dplyr)
 
-# targetDataTrain <- read.csv("proposal/TADPOLE_TargetData_train.csv")
-# 
+#targetDataTest <- read.csv("proposal/TADPOLE_PredictTargetData_test.csv")
+
+# This is the baseline script. In this script, I use the examinations from TADPOLE_InputData.csv to fill in predictions for the diagnosis, ADAS13, normalized ventricle size, and MMSE on future examination dates for patients. Most (possibly all) patients will have multiple future dates to predict.
+
+#inputTrainingData <- read.csv("proposal/TADPOLE_InputData.csv")
+
+targetDataTrain <- read.csv("proposal/TADPOLE_TargetData_train.csv")
+
 targetDataValidation <- read.csv("proposal/TADPOLE_TargetData_validation_pre_processed.csv")
-# 
-# targetDataTest <- read.csv("proposal/TADPOLE_PredictTargetData_test.csv")
-# 
-# targetBaseline <- data.frame(names(targetDataValidation))
-# 
-# targetBaseline$Date <- targetDataValidation$Date
-# targetBaseline$PTID_Key <- targetDataValidation$PTID_Key
 
 ## Create empty data frame
 dfPtidDates <- function(dataSet){
@@ -18,6 +17,7 @@ dfPtidDates <- function(dataSet){
   return(newDataSet)
 }
 
+# Given a set of exams, find the most recent non-null value. Often, there will be empty fields in a set of exams. For the move-forward baseline method, we only care about the most-recent exam.
 recentNonNAN <- function(v){
   NonNAindex <- which(!is.na(v))
   firstNonNA <- min(NonNAindex)
@@ -25,14 +25,11 @@ recentNonNAN <- function(v){
 }
 
 fillFrame <- function(emptyValidation, exams, index){
-  
-  if (index == 160) {
-     print("hello")
-  }
-  
   # Find PTID
   ptid <- exams$PTID_Key[1]
+  
   # Find most recent dx change
+  # Because dxChange is a measure of how someone's diagnosis has changed, we use it to fill in the dianostic values.
   dxChange <- recentNonNAN(exams$DXCHANGE)
   if ( dxChange %in% c(1,7,9)) {
     emptyValidation[emptyValidation$PTID_Key == ptid,]$CN_Diag = 1
@@ -55,13 +52,12 @@ fillFrame <- function(emptyValidation, exams, index){
   # Find most recent ADAS13
   ADAS13 <- recentNonNAN(exams$ADAS13)
   emptyValidation[emptyValidation$PTID_Key == ptid,]$ADAS13 = ADAS13
-  # Find most recent Ventricles
+  
+  # Find most recent Ventricles, wholebrain, then normalize
   ventricles <- recentNonNAN(exams$Ventricles)
-  
-  # Find most recent whole brain
   wholeBrain <- recentNonNAN(exams$WholeBrain)
-  
   emptyValidation[emptyValidation$PTID_Key == ptid,]$Ventricles_Norm = ventricles / wholeBrain
+  
   # Find most recent MMSE
   MMSE <- recentNonNAN(exams$MMSE)
   emptyValidation[emptyValidation$PTID_Key == ptid,]$MMSE = MMSE
@@ -81,6 +77,7 @@ validationToFill <- dfPtidDates(targetDataValidation)
 patients <- unique(validationToFill$PTID)
 
 for (index in c(1:length(patients))){
+  
   currentPatientExams <- inputTrainingData[inputTrainingData$PTID_Key == patients[index],]
   currentPatientExams <- currentPatientExams[!is.na(currentPatientExams$PTID_Key), ]
   currentPatientExams[order(as.Date(as.character(currentPatientExams$EXAMDATE), format="%m/%d/%y"), decreasing = TRUE),]
@@ -97,3 +94,35 @@ for (index in c(1:length(patients))){
 
 
 
+# Find patients in training and validation datasets
+patientsTraining <- unique(targetDataTrain$PTID)
+patientsValidation <- unique(targetDataValidation$PTID)
+
+# Find patient exams for training and 
+patientExamsTraining <- inputTrainingData[inputTrainingData$PTID_Key %in%  patientsTraining,]
+patientExamsValidation <- inputTrainingData[inputTrainingData$PTID_Key %in%  patientsValidation,]
+
+# find inputs for the RNN
+df <- patientExamsTraining
+patientsTrainingIn <- data.frame(Date = df$EXAMDATE, PTID_Key = df$PTID_Key, DXCHANG = df$DXCHANGE, ADAS13 = df$ADAS13, Ventricular_Volume = df$Ventricles, Brain_Volume = df$WholeBrain, MMSE = df$MMSE, AGE = df$AGE, SEX = df$PTGENDER, APOE = df$APOE4)
+write.csv(patientsTrainingIn, file = "trainingInputsRNN.csv")
+
+# Find validation inputs for the RNN
+df <- patientExamsValidation
+patientsValidationIn <- data.frame(Date = df$EXAMDATE, PTID_Key = df$PTID_Key, DXCHANG = df$DXCHANGE, ADAS13 = df$ADAS13, Ventricular_Volume = df$Ventricles, Brain_Volume = df$WholeBrain, MMSE = df$MMSE, AGE = df$AGE, SEX = df$PTGENDER, APOE = df$APOE4)
+write.csv(patientsValidationIn, file = "validationInputsRNN.csv")
+
+
+
+
+
+
+
+
+## Junk Code
+targetBaseline <- data.frame(matrix(nrow = dim(targetDataValidation)[1], ncol = dim(targetDataValidation)[2])) 
+
+names(targetBaseline) <- names(targetDataValidation)
+
+targetBaseline$Date <- targetDataValidation$Date
+targetBaseline$PTID_Key <- targetDataValidation$PTID_Key
